c944132fdc9f43a6f61fe702ee45d9593f617fcf
yexupeng <1633662870@qq.com>
2018-09-25 19:26:36 +0800

    Update app_key_user.h

--- key/app_key_user.h
+++ key/app_key_user.h
@@ -1,1 +1,26 @@
 
+#ifndef APP_KEY_USER_H
+#define APP_KEY_USER_H
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+	 
+	 
+#include "app_key.h"
+
+
+
+extern KEY_HandleTypeDef *head;
+
+
+
+void KEY_Init(void);
+void KEY_InitTimer(void);
+void KEY_Detect_thread(void);	
+void KEY_DeInit(void);	 
+#ifdef __cplusplus
+}
+#endif
+
+#endif
